9a3cf5e6505adb175897a3d1b6db1daf40d674fa
SourceDoctor <sourcehhdoctor@googlemail.com>
2023-08-15 23:19:33 +0200

    configurable snmp extend directory

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,8 @@
 class librenms (
   Boolean $use_local_agents = true,
-  String $snmpd_dir = $librenms::params::snmpd_dir
-  String $snmpd_extend_dir = $librenms::params::snmpd_extend_dir
-  String $sudo_bin = $librenms::params::sudo_bin
+  String $snmpd_dir = $librenms::params::snmpd_dir,
+  String $snmpd_extend_dir = $librenms::params::snmpd_extend_dir,
+  String $sudo_bin = $librenms::params::sudo_bin,
 
 ) inherits librenms::params {
 
