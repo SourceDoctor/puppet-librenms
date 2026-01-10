0aa5a538988574ab21614316dc221a69100257a4
SourceDoctor <sourcehhdoctor@googlemail.com>
2026-01-10 22:41:29 +0100

    update ntp-server agent script

--- manifests/applications/ntp_server.pp
+++ manifests/applications/ntp_server.pp
@@ -10,22 +10,14 @@ class librenms::applications::ntp_server (
         $file_existance = 'absent'
     }
 
-    file {"${snmpd_dir}/ntp-server.conf":
-        ensure  => $file_existance,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0755',
-        content => template('librenms/ntp-server.conf.erb'),
-    }
-
     create_resources(librenms::fetch, {
-            'ntp-server.sh' => {
+            'ntp-server.py' => {
             use => $enabled,
     }})
 
     librenms::snmpd{'ntp-server':
         use    => $enabled,
-        script => 'ntp-server.sh'
+        script => 'ntp-server.py'
     }
 }
 
